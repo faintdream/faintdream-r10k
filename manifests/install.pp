5573785016c00da6c5c7d2896c7c335d871970df
akash dubey <dubey.akash@gmail.com>
2019-10-12 00:06:48 +0000

    ignore exec if r10k already present.

--- manifests/install.pp
+++ manifests/install.pp
@@ -1,6 +1,7 @@
 class r10k::install {
   exec { 'gem install r10k':
-    path => '/opt/puppetlabs/puppet/bin',
+    path    => '/opt/puppetlabs/puppet/bin',
+    creates => '/opt/puppetlabs/puppet/bin/r10k'
   }
   
 
